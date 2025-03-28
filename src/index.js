8242bbd17df30e4b8291dbdd0d1dd233ba1bbd6b
fanging123 <149152681+fanging123@users.noreply.github.com>
2025-03-27 21:14:20 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  console.error('Root element not found! Check your HTML file.');
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  try {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+  } catch (error) {
+    console.error('Error rendering app:', error);
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
